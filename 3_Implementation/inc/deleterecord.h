6a45b652fb49a6dbaa21d17be7bf2194b8ef3ff9
Amisha <amishagohad99@gmail.com>
2022-03-03 22:57:21 +0530

    updated code

--- 3_Implementation/inc/deleterecord.h
+++ 3_Implementation/inc/deleterecord.h
@@ -2,9 +2,8 @@
 #define Func7
 #include <stdio.h>
 #include<stdlib.h>
-
 #include<string.h>
-
+#include <curses.h>
 // declares a function
 void deleterecord();
 #endif
--- 3_Implementation/inc/editrecord.h
+++ 3_Implementation/inc/editrecord.h
@@ -2,8 +2,7 @@
 #define Func3
 #include <stdio.h>
 #include<stdlib.h>
-
-
+#include <curses.h>
 
 #include<string.h>
 // declares a function
