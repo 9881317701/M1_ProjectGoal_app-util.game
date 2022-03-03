fa4735eefdc2d139a32bbb9ed3310923b08a8c58
Amisha <amishagohad99@gmail.com>
2022-03-03 22:38:50 +0530

    updaated

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,6 +1,4 @@
 #include<stdio.h>
-
-#include<conio.h>
 #include<string.h>
 #include<stdlib.h>
 #include"inc/addrecord.h" //
--- 3_Implementation/src/deleterecord.h
+++ 3_Implementation/src/deleterecord.h
@@ -2,7 +2,6 @@
 #define Func7
 #include <stdio.h>
 #include<stdlib.h>
-#include<conio.h>
 #include<string.h>
 
 // declares a function
